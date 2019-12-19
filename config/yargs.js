e37d3126db8498730aaa0009e2cd37a5b1665980
Hector Ortiz <hectorzitro92@gmail.com>
2019-12-18 22:37:23 -0600

    Primer comit

---
+++ config/yargs.js
@@ -0,0 +1,28 @@
+const descripcion = {
+
+    demand: true,
+    alias: 'd'
+
+};
+
+const completado = {
+    default: true,
+    alias: 'c'
+};
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualizar el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'borra una tarea', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
